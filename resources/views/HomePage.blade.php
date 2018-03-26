85695acb8c4d3c7c833996359448c0da2d168b0d
Gilles Vaste-Computer <gilles.leenders@gmail.com>
2018-03-26 11:13:22 +0200

    Login en post system volledig afgewerkt

---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>DGR Site</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HomePage');
 });
+
+Route::resource('posts' , 'PostsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
